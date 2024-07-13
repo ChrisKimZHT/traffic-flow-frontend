350f6b86b14c927251fd49a27f16a9cc220fbd5b
ChrisKim <1515748548@qq.com>
2024-07-13 13:56:48 +0800

    [feat] 流量分析页面

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import VideoView from '../view/VideoView';
 import VideoDetailView from '../view/VideoDetailView';
 import TaskView from '../view/TaskView';
+import StatView from '../view/StatView';
 
 
 const Router = () => {
@@ -11,6 +12,7 @@ const Router = () => {
       <Route path='/' element={<></>} />
       <Route path='/video' element={<VideoView />} />
       <Route path='/video/:videoId' element={<VideoDetailView />} />
+      <Route path='/stat' element={<StatView />} />
       <Route path='/task' element={<TaskView />} />
     </Routes>
   );
---
+++ src/view/StatView.jsx
@@ -0,0 +1,105 @@
+import { AreaChartOutlined, RedoOutlined } from '@ant-design/icons';
+import React, { useEffect, useState } from 'react';
+import './StatView.scss';
+import { App, Table, Button, Row, Col } from 'antd';
+import service from '../service/service';
+
+const StatView = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tableData, setTableData] = useState([]);
+  const { message, notification } = App.useApp();
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [selectedVideoId, setSelectedVideoId] = useState(null);
+
+  const columns = [
+    {
+      title: '#',
+      dataIndex: 'videoId',
+      key: 'videoId',
+    },
+    {
+      title: '标题',
+      dataIndex: 'title',
+      key: 'title',
+    },
+    {
+      title: '描述',
+      dataIndex: 'description',
+      key: 'description',
+    },
+  ];
+
+  const refreshData = () => {
+    setIsLoading(true);
+    service.video.list().then(res => {
+      let outputData = res.data.data;
+      outputData = outputData.filter(item => item.statProcessed === 2);
+      for (let i = 0; i < outputData.length; i++) {
+        outputData[i].key = i;
+      }
+      setTableData(outputData);
+      setIsLoading(false);
+    }).catch(err => {
+      notification.error({
+        message: '获取数据失败',
+        description: `${err}`
+      });
+    });
+  }
+
+  const updateSelectedData = (selectedRow) => {
+    const videoId = tableData[selectedRow].videoId;
+    setSelectedVideoId(videoId);
+  }
+
+  // eslint-disable-next-line
+  useEffect(() => { refreshData(); }, []);
+
+  const rowSelection = {
+    type: 'radio',
+    selectedRowKeys,
+    onChange: (selectedRowKeys) => {
+      setSelectedRowKeys(selectedRowKeys);
+      updateSelectedData(selectedRowKeys);
+    },
+  }
+
+  return (
+    <div className='stat-view'>
+      <div className='head-line'>
+        <div className='title'><AreaChartOutlined /> 流量分析</div>
+        <div className='operation'>
+          <Button type='primary' className='btn' onClick={refreshData} shape='circle'><RedoOutlined /></Button>
+        </div>
+      </div>
+      <Row className='row'>
+        <Col span={8} className='col-left'>
+          <Table
+            className='table'
+            loading={isLoading}
+            columns={columns}
+            dataSource={tableData}
+            bordered={true}
+            rowSelection={rowSelection}
+            pagination={{ position: ["bottomCenter"] }}
+          />
+        </Col>
+        <Col span={16} className='col-right'>
+          <Row className='img-row'>
+            <Col span={12} className='img-col-left'>
+              <img className='img' src={`${window.baseURL}stat/resultPlot?videoId=${selectedVideoId}&plotName=count_per_frame.png`} alt='stat' />
+            </Col>
+            <Col span={12} className='img-col-right'>
+              <img className='img' src={`${window.baseURL}stat/resultPlot?videoId=${selectedVideoId}&plotName=count_prefix_sum.png`} alt='stat' />
+            </Col>
+          </Row>
+          <video className='video' controls preload='auto' key={selectedVideoId}>
+            <source src={`${window.baseURL}stat/resultVideo?videoId=${selectedVideoId}`} type='video/mp4' />
+          </video>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default StatView;
